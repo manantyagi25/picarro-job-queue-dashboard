6647f20c7342e7c787f796db2161a16421697a92
RohaanoaZoro <rohaan.almeida@gmail.com>
2026-03-16 14:35:50 +0530

    Fix react query provider and next config

--- src/app/react-query-provider.tsx
+++ src/app/react-query-provider.tsx
@@ -1,10 +1,8 @@
 "use client";
 
 import { type ReactNode } from "react";
-import {
-  QueryClientProvider,
-  ReactQueryDevtools
-} from "@tanstack/react-query-devtools";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { queryClient } from "@/lib/react-query-client";
 
 interface ReactQueryProviderProps {
